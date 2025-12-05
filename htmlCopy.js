b6a55d03e59846007bd595d1370c89c351f601f5
cymYoung <1269567664@qq.com>
2025-12-05 11:20:50 +0800

    chore: init

---
+++ htmlCopy.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         元素属性快速复制
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  鼠标悬浮高亮元素，并一键复制类名、文本、HTML 等属性（按钮栏固定在左上角）
+// @author       Young
+// @match        *://*/*
+// @grant        GM_addStyle
+// @grant        GM_addElement
+// @grant        GM_setClipboard
+// @grant        GM_registerMenuCommand
+// @noframes
+// ==/UserScript==
